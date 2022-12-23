ed5342e415abc2cc145eca5bc0c3623865c31f35
Mohammad Omidvar <moa39@sfu.ca>
2022-12-22 20:56:03 -0800

    Fix visitors to match the new version of rust

--- new/leafc_new/src/visit.rs
+++ new/leafc_new/src/visit.rs
@@ -15,12 +15,7 @@ trait TerminatorKindMutVisitor<'tcx, T> {
 
     fn visit_goto(&mut self, target: &mut BasicBlock) -> T;
 
-    fn visit_switch_int(
-        &mut self,
-        discr: &mut Operand<'tcx>,
-        switch_ty: Ty<'tcx>,
-        targets: &mut SwitchTargets,
-    ) -> T;
+    fn visit_switch_int(&mut self, discr: &mut Operand<'tcx>, targets: &mut SwitchTargets) -> T;
 
     fn visit_resume(&mut self) -> T;
 
@@ -49,7 +44,8 @@ trait TerminatorKindMutVisitor<'tcx, T> {
         &mut self,
         func: &mut Operand<'tcx>,
         args: &mut Vec<Operand<'tcx>>,
-        destination: &mut Option<(Place<'tcx>, BasicBlock)>,
+        destination: &mut Place<'tcx>,
+        target: &mut Option<BasicBlock>,
         cleanup: &mut Option<BasicBlock>,
         from_hir_call: bool,
         fn_span: Span,
@@ -89,7 +85,7 @@ trait TerminatorKindMutVisitor<'tcx, T> {
 
     fn visit_inline_asm(
         &mut self,
-        template: &'tcx mut [InlineAsmTemplatePiece],
+        template: &'tcx [InlineAsmTemplatePiece],
         operands: &mut Vec<InlineAsmOperand<'tcx>>,
         options: &mut InlineAsmOptions,
         line_spans: &'tcx [Span],
@@ -99,84 +95,79 @@ trait TerminatorKindMutVisitor<'tcx, T> {
 
     fn super_visit_terminator_kind(&mut self, kind: &mut TerminatorKind<'tcx>) -> T {
         match kind {
-            TerminatorKind::Goto { target } => self.visit_goto(&mut target),
+            TerminatorKind::Goto { ref mut target } => self.visit_goto(target),
             TerminatorKind::SwitchInt {
                 discr,
-                switch_ty,
-                targets,
-            } => self.visit_switch_int(discr, *switch_ty, &mut targets),
+                ref mut targets,
+            } => self.visit_switch_int(discr, targets),
             TerminatorKind::Resume => self.visit_resume(),
             TerminatorKind::Abort => self.visit_abort(),
             TerminatorKind::Return => self.visit_return(),
             TerminatorKind::Unreachable => self.visit_unreachable(),
             TerminatorKind::Drop {
-                place,
-                target,
-                unwind,
-            } => self.visit_drop(&mut place, &mut target, &mut unwind),
+                ref mut place,
+                ref mut target,
+                ref mut unwind,
+            } => self.visit_drop(place, target, unwind),
             TerminatorKind::DropAndReplace {
-                place,
-                value,
-                target,
-                unwind,
-            } => self.visit_drop_and_replace(&mut place, &mut value, &mut target, &mut unwind),
+                ref mut place,
+                ref mut value,
+                ref mut target,
+                ref mut unwind,
+            } => self.visit_drop_and_replace(place, value, target, unwind),
             TerminatorKind::Call {
                 func,
                 args,
-                destination,
-                cleanup,
+                ref mut destination,
+                ref mut target,
+                ref mut cleanup,
                 from_hir_call,
                 fn_span,
             } => self.visit_call(
                 func,
                 args,
-                &mut destination,
-                &mut cleanup,
+                destination,
+                target,
+                cleanup,
                 *from_hir_call,
                 *fn_span,
             ),
             TerminatorKind::Assert {
-                cond,
-                expected,
-                msg,
-                target,
-                cleanup,
-            } => self.visit_assert(
-                &mut cond,
-                &mut expected,
-                &mut msg,
-                &mut target,
-                &mut cleanup,
-            ),
+                ref mut cond,
+                ref mut expected,
+                ref mut msg,
+                ref mut target,
+                ref mut cleanup,
+            } => self.visit_assert(cond, expected, msg, target, cleanup),
             TerminatorKind::Yield {
-                value,
-                resume,
-                resume_arg,
-                drop,
-            } => self.visit_yield(&mut value, &mut resume, &mut resume_arg, &mut drop),
+                ref mut value,
+                ref mut resume,
+                ref mut resume_arg,
+                ref mut drop,
+            } => self.visit_yield(value, resume, resume_arg, drop),
             TerminatorKind::GeneratorDrop => self.visit_generator_drop(),
             TerminatorKind::FalseEdge {
-                real_target,
-                imaginary_target,
-            } => self.visit_false_edge(&mut real_target, &mut imaginary_target),
+                ref mut real_target,
+                ref mut imaginary_target,
+            } => self.visit_false_edge(real_target, imaginary_target),
             TerminatorKind::FalseUnwind {
-                real_target,
-                unwind,
-            } => self.visit_false_unwind(&mut real_target, &mut unwind),
+                ref mut real_target,
+                ref mut unwind,
+            } => self.visit_false_unwind(real_target, unwind),
             TerminatorKind::InlineAsm {
                 template,
+                ref mut operands,
+                ref mut options,
+                line_spans,
+                ref mut destination,
+                ref mut cleanup,
+            } => self.visit_inline_asm(
+                &template,
                 operands,
                 options,
                 line_spans,
                 destination,
                 cleanup,
-            } => self.visit_inline_asm(
-                &mut template,
-                &mut operands,
-                &mut options,
-                line_spans,
-                &mut destination,
-                &mut cleanup,
             ),
         }
     }
@@ -215,17 +206,6 @@ trait DefaultTerminatorKindMutVisitor<'tcx>: TerminatorKindMutVisitor<'tcx, ()> 
         value: &mut Operand<'tcx>,
         target: &mut BasicBlock,
         unwind: &mut Option<BasicBlock>,
-    ) {
-    }
-
-    fn visit_call(
-        &mut self,
-        func: &mut Operand<'tcx>,
-        args: &mut Vec<Operand<'tcx>>,
-        destination: &mut Option<(Place<'tcx>, BasicBlock)>,
-        cleanup: &mut Option<BasicBlock>,
-        from_hir_call: bool,
-        fn_span: Span,
     ) {
     }
 
@@ -337,6 +317,8 @@ trait RvalueMutVisitor<'tcx, T> {
 
     fn visit_shallow_init_box(&mut self, operand: &mut Operand<'tcx>, ty: &mut Ty<'tcx>) -> T;
 
+    fn visit_copy_for_deref(&mut self, place: &mut Place<'tcx>) -> T;
+
     fn super_visit_rvalue(&mut self, rvalue: &mut Rvalue<'tcx>) -> T {
         match rvalue {
             Rvalue::Use(operand) => self.visit_use(operand),
@@ -353,6 +335,7 @@ trait RvalueMutVisitor<'tcx, T> {
             Rvalue::Discriminant(place) => self.visit_discriminant(place),
             Rvalue::Aggregate(kind, operands) => self.visit_aggregate(kind, operands),
             Rvalue::ShallowInitBox(operand, ty) => self.visit_shallow_init_box(operand, ty),
+            Rvalue::CopyForDeref(place) => self.visit_copy_for_deref(place),
         }
     }
 }
@@ -412,4 +395,6 @@ trait DefaultRvalueMutVisitor<'tcx>: RvalueMutVisitor<'tcx, ()> {
     }
 
     fn visit_shallow_init_box(&mut self, operand: &mut Operand<'tcx>, ty: &mut Ty<'tcx>) {}
+
+    fn visit_copy_for_deref(&mut self, place: &mut Place<'tcx>) {}
 }
