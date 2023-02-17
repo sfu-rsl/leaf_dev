17d3577a10d9249d429d6c424de85c317c89cc49
Mohammad Omidvar <moa39@sfu.ca>
2023-02-16 17:19:48 -0800

    Add a first sketch of runtime

--- pri/src/runtime.rs
+++ pri/src/runtime.rs
@@ -1,12 +1,79 @@
-pub trait Runtime : Sized {
-    fn assign<T: Assignment>() -> T;
+use crate::{BinaryOp, Local, UnaryOp};
+
+pub trait Runtime: Sized {
+    type PlaceHandler: PlaceHandler;
+    type AssignmentHandler: AssignmentHandler;
+
+    fn place(&mut self) -> Self::PlaceHandler;
+
+    fn assign_to(dest: <Self::PlaceHandler as PlaceHandler>::Place) -> Self::AssignmentHandler;
 
     fn branch<T: Branching>() -> T;
 
     fn function<T: Function>() -> T;
 }
 
-pub trait Assignment {}
+pub trait PlaceHandler {
+    type Place;
+
+    type ProjectionHandler: PlaceProjectionHandler;
+
+    fn of_local(&mut self, local: Local) -> Self::Place;
+
+    fn project_on(&mut self, place: Self::Place) -> Self::ProjectionHandler;
+}
+
+pub trait PlaceProjectionHandler {
+    type Place;
+
+    fn deref(&mut self) -> Self::Place;
+
+    fn for_field(&mut self, field: u32) -> Self::Place;
+
+    fn at_index(&mut self, index: Self::Place) -> Self::Place;
+
+    fn at_constant_index(&mut self, offset: u64, min_length: u64, from_end: bool) -> Self::Place;
+
+    fn subslice(&mut self, from: u64, to: u64, from_end: bool) -> Self::Place;
+
+    fn downcast(&mut self, variant_index: u32) -> Self::Place;
+
+    fn opaque_cast(&mut self) -> Self::Place;
+}
+
+pub trait AssignmentHandler {
+    type Place;
+    type Operand;
+
+    fn use_of(operand: Self::Operand);
+
+    fn repeat_of(operand: Self::Operand, count: usize);
+
+    fn ref_to(place: Self::Place, is_mutable: bool);
+
+    fn thread_local_ref_to();
+
+    fn address_of(place: Self::Place, is_mutable: bool);
+
+    fn len_of(place: Self::Place);
+
+    fn numeric_cast_of(operand: Self::Operand, is_to_float: bool, size: usize);
+
+    fn cast_of();
+
+    fn binary_op_between(
+        operator: BinaryOp,
+        first: Self::Operand,
+        second: Self::Operand,
+        checked: bool,
+    );
+
+    fn unary_op_on(operator: UnaryOp, operand: Self::Operand);
+
+    fn discriminant_of(place: Self::Place);
+
+    fn array_from(items: &[Self::Operand]);
+}
 
 pub trait Branching {}
 
