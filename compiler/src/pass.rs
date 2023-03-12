e1c5bb4e43401390aaad64cfd0d96ec54c3157af
Bryan Niwa <bniwa@sfu.ca>
2023-03-11 16:49:58 -0800

    Remove unused variable

--- compiler/src/pass.rs
+++ compiler/src/pass.rs
@@ -165,7 +165,7 @@ where
         let destination = self.call_adder.reference_place(place);
         self.call_adder
             .assign(destination)
-            .set_discriminant(destination, variant_index)
+            .set_discriminant(variant_index)
     }
 
     fn visit_deinit(&mut self, place: &Place<'tcx>) -> () {
