2b76f2bfd5ceb465a44e8ea7432c9bd2a51c0221
Mohammad Omidvar <moa39@sfu.ca>
2023-04-25 14:15:09 -0700

    Format samples

--- samples/terminator/assert/overflow.rs
+++ samples/terminator/assert/overflow.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut x: usize = 10;
-    if 10 % 3 == 2 { x = 1; }
+    if 10 % 3 == 2 {
+        x = 1;
+    }
     let y = x - 1;
 }
--- samples/terminator/assert/simple_fail.rs
+++ samples/terminator/assert/simple_fail.rs
@@ -1,5 +1,7 @@
 fn main() {
     let mut y = 0;
-    if 10 % 3 == 2 { y = 1; }
+    if 10 % 3 == 2 {
+        y = 1;
+    }
     let x = 10 / y;
 }
