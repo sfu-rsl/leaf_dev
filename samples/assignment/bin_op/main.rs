f10e9bc884974d9eed4eb141d6cf40b51ebebb24
Mohammad Omidvar <moa39@sfu.ca>
2023-06-14 14:48:36 -0700

    Clean up the binary operations sample

--- samples/assignment/bin_op/main.rs
+++ samples/assignment/bin_op/main.rs
@@ -1,27 +1,51 @@
+macro_rules! do_arithmetic {
+    ($expression: expr) => {
+        let result = $expression;
+        use_num(result);
+    };
+}
+
+macro_rules! do_bitwise {
+    ($expression: expr) => {
+        do_arithmetic!($expression);
+    };
+}
+
+macro_rules! do_cmp {
+    ($expression: expr) => {
+        let result = $expression;
+        use_bool(result);
+    };
+}
+
 fn main() {
     let a = get_num();
     let b = get_num() + 5;
 
-    let x = a + b;
-    let x = a - b;
-    let x = a * b;
-    let x = a / b;
-    let x = a % b;
+    do_arithmetic!(a + b);
+    do_arithmetic!(a - b);
+    do_arithmetic!(a * b);
+    do_arithmetic!(a / b);
+    do_arithmetic!(a % b);
 
-    let x = a ^ b;
-    let x = a & b;
-    let x = a | b;
-    let x = a << b;
-    let x = a >> b;
+    do_bitwise!(a ^ b);
+    do_bitwise!(a & b);
+    do_bitwise!(a | b);
+    do_bitwise!(a << b);
+    do_bitwise!(a >> b);
 
-    let x = a == b;
-    let x = a < b;
-    let x = a <= b;
-    let x = a != b;
-    let x = a >= b;
-    let x = a > b;
+    do_cmp!(a == b);
+    do_cmp!(a < b);
+    do_cmp!(a <= b);
+    do_cmp!(a != b);
+    do_cmp!(a >= b);
+    do_cmp!(a > b);
 }
 
 fn get_num() -> isize {
     10
 }
+
+fn use_num(_: isize) {}
+
+fn use_bool(_: bool) {}
