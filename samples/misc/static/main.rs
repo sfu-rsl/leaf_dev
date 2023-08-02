6fb88f67dd6826a4fb2199cfacc951e08db9a0c8
Mohammad Omidvar <m_omidvart@sfu.ca>
2023-08-02 00:25:41 +0000

    Add a sample for static items

---
+++ samples/misc/static/main.rs
@@ -0,0 +1,31 @@
+static PRIMITIVE: i32 = 10;
+
+static PRIMITIVE_REF: &i32 = &PRIMITIVE;
+
+static ADT: TestStruct = TestStruct { a: 7, b: 11 };
+
+static ADT_REF: &TestStruct = &ADT;
+
+// Not supported yet
+// static FN_PTR: fn() -> () = bar;
+
+fn main() {
+    foo(PRIMITIVE);
+    foo(PRIMITIVE_REF);
+    foo(&ADT);
+    foo(ADT_REF);
+
+    // Not supported yet
+    // foo(FN_PTR);
+}
+
+struct TestStruct {
+    a: i32,
+    b: i32,
+}
+
+fn foo<T>(x: T) {
+    bar();
+}
+
+fn bar() {}
