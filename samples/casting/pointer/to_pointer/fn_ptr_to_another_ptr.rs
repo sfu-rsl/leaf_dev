a516ebcbf0109c6dc1b2fb218b882acdf49f222a
Khang Le <tkhang1999@gmail.com>
2023-12-14 11:53:02 -0800

    fix samples

--- samples/casting/pointer/to_pointer/fn_ptr_to_another_ptr.rs
+++ samples/casting/pointer/to_pointer/fn_ptr_to_another_ptr.rs
@@ -1,6 +1,9 @@
 fn main() {
     let a = foo;
     let b = a as *const ();
+    bar(b);
 }
 
 fn foo() {}
+
+fn bar<T>(x: T) {}
--- samples/casting/pointer/to_pointer/ptr_to_another_ptr.rs
+++ samples/casting/pointer/to_pointer/ptr_to_another_ptr.rs
@@ -1,7 +1,7 @@
 fn main() {
     let x = 10;
     let y = &x as *const i32;
-    let z = y as *const i64;
+    let z = y as *const u32;
     foo(z);
 }
 
