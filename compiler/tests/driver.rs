167f76436aeafa06a78a34297dd3808d987a4086
Mohammad Omidvar <m_omidvart@sfu.ca>
2024-03-13 17:29:49 -0700

    #323 Add support for testing compilation of crates (#396)
    * test folder projects
    
    * rename
    
    * simplify
    
    * ignore failing tests
    
    * simplify
    
    * Clean up macros
    
    * Clean up driver
    
    ---------
    
    Co-authored-by: Frédéric Tuong <ftuong@sfu.ca>

--- compiler/tests/driver.rs
+++ compiler/tests/driver.rs
@@ -2,35 +2,28 @@ use std::fs;
 use std::path::Path;
 use std::process::Command;
 
-use macros::gen_tests;
+use macros::{gen_tests_rs, gen_tests_toml};
 
-#[gen_tests("samples")]
-fn test_compile(fic: &str) {
-    let output_dir = {
-        use std::time::SystemTime;
-        let mut path = std::env::temp_dir();
-        path.push(
-            SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_nanos()
-                .to_string(),
-        );
-        path
-    };
-    fs::create_dir_all(&output_dir).unwrap();
+const ENV_LEAFC_LOG: &str = "LEAFC_LOG";
+const ENV_RUSTC: &str = "RUSTC";
+const ENV_RUST_FLAGS: &str = "RUSTFLAGS";
+const ENV_RUST_BACKTRACE: &str = "RUST_BACKTRACE";
+
+const LOG_CONFIG: &str = "off";
+
+// Disable warnings to avoid polluting the output
+const RUST_FLAGS: &str = "-Awarnings";
+
+#[gen_tests_rs("samples")]
+fn test_compile_rs(fic: &str) {
+    let output_dir = create_temp_dir();
 
     let status = Command::new(env!("CARGO_BIN_EXE_leafc"))
-        .arg(
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join(fic),
-        )
-        // Disable warnings to avoid polluting the output
-        .arg("-Awarnings")
+        .arg(get_test_path(fic))
+        .arg(RUST_FLAGS)
         .arg("--edition=2021")
-        .env("LEAFC_LOG", "off")
+        .env(ENV_LEAFC_LOG, LOG_CONFIG)
+        .env(ENV_RUST_BACKTRACE, "1")
         .current_dir(&output_dir)
         .status()
         .expect("Failed to spawn and wait for command termination");
@@ -45,4 +38,52 @@ fn test_compile(fic: &str) {
     } else {
         let _ = fs::remove_dir_all(output_dir);
     }
+}
+
+#[gen_tests_toml("samples/crates")]
+fn test_compile_toml(source_dir: &str) {
+    let output_dir = create_temp_dir();
+
+    let status = Command::new("cargo")
+        .arg("build")
+        .env("CARGO_TARGET_DIR", &output_dir)
+        .env(ENV_LEAFC_LOG, LOG_CONFIG)
+        .env(ENV_RUSTC, env!("CARGO_BIN_EXE_leafc"))
+        .env(ENV_RUST_FLAGS, RUST_FLAGS)
+        .env(ENV_RUST_BACKTRACE, "1")
+        .current_dir(get_test_path(source_dir))
+        .status()
+        .expect("Failed to spawn and wait for command termination");
+
+    if !status.success() {
+        panic!(
+            "Failed to compile {} with exit code: {}, output available in {}",
+            source_dir,
+            status.code().unwrap_or(-1),
+            output_dir.display()
+        );
+    } else {
+        let _ = fs::remove_dir_all(output_dir);
+    }
+}
+
+fn create_temp_dir() -> std::path::PathBuf {
+    use std::time::SystemTime;
+    let mut path = std::env::temp_dir();
+    path.push(
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            .to_string(),
+    );
+    fs::create_dir_all(&path).unwrap();
+    path
+}
+
+fn get_test_path(name: &str) -> std::path::PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join(name)
 }
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,12 @@
 use ignore::WalkBuilder;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use std::collections::HashSet;
 use std::path::{Path, MAIN_SEPARATOR};
-use syn::{parse_macro_input, Ident, ItemFn, LitStr};
+use syn::{parse_macro_input, ItemFn, LitStr};
 
 #[proc_macro_attribute]
-pub fn gen_tests(input_path: TokenStream, input_fn: TokenStream) -> TokenStream {
+pub fn gen_tests_rs(input_path: TokenStream, input_fn: TokenStream) -> TokenStream {
     let mut tests = proc_macro2::TokenStream::new();
-    let mut tests_name: HashSet<Ident> = HashSet::new();
     let test_fn = parse_macro_input!(input_fn as ItemFn);
     let test_name_fn = &test_fn.sig.ident;
 
@@ -18,56 +16,21 @@ pub fn gen_tests(input_path: TokenStream, input_fn: TokenStream) -> TokenStream 
         .ignore(false)
         .add_custom_ignore_filename(ignore_filename.clone())
         .build()
+        .filter_map(|entry| entry.ok())
     {
-        match entry {
-            Ok(entry) => {
-                let path = entry.path();
-                if path.is_file() {
-                    let file_name = path.file_name().expect("invalid file name").to_string_lossy();
-                    if
-                        if let Some(rs_file_name) = file_name.strip_suffix(".rs") {
-                            if rs_file_name.is_empty() {
-                                true
-                            } else {
-                                let test_name = format_ident!(
-                                    "{}",
-                                    path.parent().expect("invalid parent")
-                                        .join(rs_file_name).to_string_lossy()
-                                        .replace("-", "_")
-                                        .replace(MAIN_SEPARATOR, "__")
-                                );
-                                if tests_name.contains(&test_name) {
-                                    true
-                                } else {
-                                    tests_name.insert(test_name.clone());
-                                    let test_path = &path.display().to_string();
-                                    tests.extend(quote! {
-                                        #[test]
-                                        fn #test_name() {
-                                            #test_name_fn(#test_path)
-                                        }
-                                    });
-                                    false
-                                }
-                            }
-                        } else {
-                            true
-                        }
-                    {
-                        if file_name == ignore_filename {
-                            eprintln!("note: loaded ignore rules `{}`", path.display())
-                        } else {
-                            eprintln!("note: ignoring testing `{}`", path.display())
-                        }
-                    }
-                    
-                }
-            }
-            Err(err) => {
-                eprintln!("error: expecting an existing file or directory");
-                panic!("{}", err);
-            }
+        let path = entry.path();
+        if !path.extension().is_some_and(|ext| ext == "rs") {
+            continue;
         }
+
+        let test_name = format_ident!("{}", get_test_name(path));
+        let test_path = &path.display().to_string();
+        tests.extend(quote! {
+            #[test]
+            fn #test_name() {
+                #test_name_fn(#test_path)
+            }
+        });
     }
 
     TokenStream::from(quote! {
@@ -77,4 +40,49 @@ pub fn gen_tests(input_path: TokenStream, input_fn: TokenStream) -> TokenStream 
             #tests
         }
     })
+}
+
+#[proc_macro_attribute]
+pub fn gen_tests_toml(input_path: TokenStream, input_fn: TokenStream) -> TokenStream {
+    let mut tests = proc_macro2::TokenStream::new();
+    let test_fn = parse_macro_input!(input_fn as ItemFn);
+    let test_name_fn = &test_fn.sig.ident;
+
+    let ignore_filename = format!(".{}.ignore", test_name_fn);
+    for entry in WalkBuilder::new(Path::new(&parse_macro_input!(input_path as LitStr).value()))
+        .hidden(false)
+        .ignore(false)
+        .add_custom_ignore_filename(ignore_filename.clone())
+        .build()
+        .filter_map(|entry| entry.ok())
+    {
+        let path = entry.path();
+        if !path.join("Cargo.toml").exists() {
+            continue;
+        }
+
+        let test_name = format_ident!("{}", get_test_name(path));
+        let test_path = &path.display().to_string();
+        tests.extend(quote! {
+            #[test]
+            fn #test_name() {
+                #test_name_fn(#test_path)
+            }
+        });
+    }
+
+    TokenStream::from(quote! {
+        mod #test_name_fn {
+            use super::*;
+            #test_fn
+            #tests
+        }
+    })
+}
+
+fn get_test_name(path: &Path) -> String {
+    path.with_extension("")
+        .to_string_lossy()
+        .replace("-", "_")
+        .replace(MAIN_SEPARATOR, "__")
 }
