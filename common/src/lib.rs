73d50f125edd897a5b88e944ac0716adab52ca41
Mohammad Omidvar <m_omidvart@sfu.ca>
2024-06-27 23:30:59 +0000

    Exclude shim and commond crates from instrumentation

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(macro_metavar_expr)]
 #![feature(core_intrinsics)]
+#![cfg_attr(not(core_build), feature(register_tool))]
+#![cfg_attr(not(core_build), register_tool(leaf_attr))]
 #![no_std]
 
 #[cfg(feature = "std")]
@@ -9,13 +11,18 @@ use std::prelude::rust_2021::*;
 
 #[cfg(feature = "config")]
 pub mod config;
-pub mod ffi;
 #[cfg(feature = "logging")]
 pub mod logging;
-pub mod pri;
 #[cfg(feature = "tyexp")]
 pub mod tyexp;
+
+#[leaf_attr::instrument(false)]
+pub mod ffi;
+#[leaf_attr::instrument(false)]
+pub mod pri;
+#[leaf_attr::instrument(false)]
 pub mod types;
+#[leaf_attr::instrument(false)]
 pub mod utils;
 
 // NOTE: For compatibility when embedded in the core library.
--- runtime/shim/src/lib.rs
+++ runtime/shim/src/lib.rs
@@ -3,6 +3,8 @@
 #![cfg_attr(not(core_build), feature(core_intrinsics))]
 #![cfg_attr(not(core_build), no_std)]
 #![cfg_attr(not(core_build), feature(rustc_attrs))]
+#![cfg_attr(not(core_build), feature(register_tool))]
+#![cfg_attr(not(core_build), register_tool(leaf_attr))]
 // -----
 #![cfg_attr(core_build, allow(missing_docs))]
 #![cfg_attr(core_build, stable(feature = "rust1", since = "1.0.0"))]
@@ -11,6 +13,7 @@
 pub mod annotations;
 
 #[cfg_attr(core_build, stable(feature = "rust1", since = "1.0.0"))]
+#[leaf_attr::instrument(false)]
 pub mod pri;
 
 #[cfg(core_build)]
