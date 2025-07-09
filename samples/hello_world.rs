fd7a14436cf5241e0c23c31617051e386976291c
Mohammad Omidvar <m_omidvart@sfu.ca>
2025-07-09 21:26:52 +0000

    Make compatible with normal compilation

--- samples/hello_world.rs
+++ samples/hello_world.rs
@@ -1,7 +1,10 @@
-use leaf::annotations::*;
-
 fn main() {
-    let x: u8 = 10.mark_symbolic();
+    let x: u8 = core::hint::black_box(10);
+    #[cfg(leafc)]
+    let x: u8 = {
+        use leaf::annotations::*;
+        x.mark_symbolic()
+    };
 
     if x < 5 {
         println!("Hello, world!");
