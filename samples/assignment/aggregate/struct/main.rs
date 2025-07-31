a245c4af39658224356f6eef47cbacd0f8c2afc2
Mohammad Omidvar <m_omidvart@sfu.ca>
2025-07-31 16:08:45 +0000

    Improve struct sample for layout coverage

--- samples/assignment/aggregate/struct/main.rs
+++ samples/assignment/aggregate/struct/main.rs
@@ -2,28 +2,43 @@ fn main() {
     let a = get_foo();
     let b = get_bar();
     let c = Baz { a, b };
+
+    if c.a.x + c.a.y < c.b.0 {
+        core::hint::black_box(foo());
+    }
 }
 
-fn get_num() -> i32 {
-    10
+fn get_x() -> u8 {
+    let x = 5u8;
+    #[cfg(leafc)]
+    let x: u8 = {
+        use leaf::annotations::*;
+        x.mark_symbolic()
+    };
+    x
 }
 
 fn get_foo() -> Foo {
-    Foo { x: 5, y: get_num() }
+    let x = get_x();
+    Foo { x, y: x + 2 }
 }
 
 fn get_bar() -> Bar {
-    Bar(5, get_num())
+    Bar(get_x(), 20)
 }
 
 struct Foo {
-    x: i32,
-    y: i32,
+    x: u8,
+    y: u8,
 }
 
-struct Bar(i32, i32);
+// The fields may be reordered.
+struct Bar(u8, u32);
 
+// The fields may be reordered.
 struct Baz {
     a: Foo,
     b: Bar,
 }
+
+fn foo() {}
