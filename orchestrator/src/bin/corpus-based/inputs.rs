67b34812881ba65756fd566136c15064d989b576
Mohammad Omidvar <m_omidvart@sfu.ca>
2025-08-14 19:18:41 +0000

    Add support for input file filters

--- orchestrator/src/bin/corpus-based/inputs.rs
+++ orchestrator/src/bin/corpus-based/inputs.rs
@@ -1,6 +1,10 @@
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
 use futures::{Stream, StreamExt, TryStream, TryStreamExt};
+use glob::Pattern;
 use tokio::{pin, task::JoinHandle};
 
 use common::{log_debug, log_info};
@@ -8,6 +12,48 @@ use common::{log_debug, log_info};
 use crate::{Input, utils::GenericError};
 
 type Result<T> = std::result::Result<T, GenericError>;
+
+#[derive(Debug, Clone)]
+struct PathFilter {
+    exclude_patterns: Arc<[Pattern]>,
+    include_patterns: Arc<[Pattern]>,
+}
+
+impl PathFilter {
+    fn new(
+        exclude_patterns: &[impl AsRef<str>],
+        include_patterns: &[impl AsRef<str>],
+    ) -> Result<Self> {
+        fn to_patterns(patterns: &[impl AsRef<str>]) -> Result<Arc<[Pattern]>> {
+            patterns
+                .into_iter()
+                .map(|p| p.as_ref())
+                .map(Pattern::new)
+                .try_collect::<Vec<_>>()
+                .map(Into::into)
+                .map_err(GenericError::from)
+        }
+        let exclude_patterns = to_patterns(exclude_patterns)?;
+        let include_patterns = to_patterns(include_patterns)?;
+
+        Ok(Self {
+            exclude_patterns,
+            include_patterns,
+        })
+    }
+
+    fn check(&self, path: &Path) -> bool {
+        if !self.include_patterns.is_empty() {
+            if !self.include_patterns.iter().any(|p| p.matches_path(path)) {
+                return false;
+            }
+        }
+        if self.exclude_patterns.iter().any(|p| p.matches_path(path)) {
+            return false;
+        }
+        true
+    }
+}
 
 mod corpus {
     use notify::Watcher;
@@ -18,24 +64,32 @@ mod corpus {
 
     pub(super) async fn read_all_files(
         dir: &Path,
+        filter: PathFilter,
     ) -> Result<impl TryStream<Ok = PathBuf, Error = GenericError> + use<>> {
         let read_dir = ReadDirStream::new(tokio::fs::read_dir(dir).await?);
-        Ok(read_dir.filter_map(async |entry| match entry {
-            Ok(entry) => {
-                let file_type = match entry.file_type().await {
-                    Ok(file_type) => file_type,
-                    Err(e) => {
-                        return Some(Err(e.into()));
+        Ok(read_dir.map(move |e| (e, filter.clone())).filter_map(
+            async |(entry, filter)| match entry {
+                Ok(entry) => {
+                    let file_type = match entry.file_type().await {
+                        Ok(file_type) => file_type,
+                        Err(e) => {
+                            return Some(Err(e.into()));
+                        }
+                    };
+                    if file_type.is_file() {
+                        let path = entry.path();
+                        if filter.check(&path) {
+                            Some(Ok(path))
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
                     }
-                };
-                if file_type.is_file() {
-                    Some(Ok(entry.path()))
-                } else {
-                    None
                 }
-            }
-            Err(e) => Some(Err(e.into())),
-        }))
+                Err(e) => Some(Err(e.into())),
+            },
+        ))
     }
 
     struct TokioSenderAdapter(mpsc::UnboundedSender<notify::Result<notify::Event>>);
@@ -48,6 +102,7 @@ mod corpus {
 
     pub(super) fn watch_dir(
         dir: &Path,
+        filter: PathFilter,
     ) -> Result<(
         impl Drop + use<>,
         impl TryStream<Ok = PathBuf, Error = GenericError> + use<>,
@@ -57,16 +112,23 @@ mod corpus {
         watcher.watch(dir, notify::RecursiveMode::NonRecursive)?;
         Ok((
             watcher,
-            UnboundedReceiverStream::new(rx).filter_map(async |event| match event {
-                Ok(event) => {
-                    if event.kind.is_create() && event.paths.len() == 1 {
-                        Some(Ok(event.paths[0].clone()))
-                    } else {
-                        None
+            UnboundedReceiverStream::new(rx)
+                .map(move |e| (e, filter.clone()))
+                .filter_map(async |(event, filter)| match event {
+                    Ok(mut event) => {
+                        if event.kind.is_create() || event.kind.is_modify() {
+                            let path = event.paths.pop().unwrap();
+                            if filter.check(&path) {
+                                Some(Ok(path))
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
                     }
-                }
-                Err(e) => Some(Err(e.into())),
-            }),
+                    Err(e) => Some(Err(e.into())),
+                }),
         ))
     }
 }
@@ -117,14 +179,19 @@ mod process {
 }
 
 pub(crate) async fn prioritized_inputs(
-    inputs_dir: PathBuf,
+    input_corpus: &super::InputCorpusArgs,
     offline: bool,
-) -> Result<(JoinHandle<()>, impl Stream<Item = Input>)> {
+) -> Result<(JoinHandle<()>, impl Stream<Item = Input> + use<>)> {
     let (processor, inputs) = process::NoOpCorpusInputProcessor::new();
 
-    let existing = corpus::read_all_files(inputs_dir.as_ref()).await?;
+    let inputs_dir = &input_corpus.dir;
+    let filter = PathFilter::new(
+        &input_corpus.exclude_patterns,
+        &input_corpus.include_patterns,
+    )?;
+    let existing = corpus::read_all_files(inputs_dir.as_ref(), filter.clone()).await?;
     let new_files = if !offline {
-        Some(corpus::watch_dir(inputs_dir.as_ref())?)
+        Some(corpus::watch_dir(inputs_dir.as_ref(), filter.clone())?)
     } else {
         None
     };
