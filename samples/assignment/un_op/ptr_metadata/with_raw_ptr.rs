7c5518d40c7e8f04f70bfc3ed7f291d6e02df838
Mohammad Omidvar <m_omidvart@sfu.ca>
2026-01-20 18:21:34 +0000

    Fix minor failures in tests

--- samples/assignment/un_op/ptr_metadata/with_raw_ptr.rs
+++ samples/assignment/un_op/ptr_metadata/with_raw_ptr.rs
@@ -1,7 +1,8 @@
 #![feature(core_intrinsics)]
 fn main() {
     let x = 10;
-    let s: *const [i32] = core::intrinsics::aggregate_raw_ptr::<*const [i32], _, _>(&x, 1);
+    let s: *const [i32] =
+        core::intrinsics::aggregate_raw_ptr::<*const [i32], _, _>(&x as *const i32, 1);
     let meta = core::intrinsics::ptr_metadata(s);
     foo(meta);
 }
